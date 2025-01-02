8a26df414ab8dfc0db6e45d563318d38327cafd7
Ashutosh-Tiwari <ashutiwari9518@gmail.com>
2025-01-02 09:42:31 +0530

    #feat: UI updates

--- src/navigation/SignIn/index.tsx
+++ src/navigation/SignIn/index.tsx
@@ -4,24 +4,50 @@ import SignInPhoneNumberScreen from "../../screens/SignIn/SignInPhoneNumberScree
 import SignInOTPVerificationScreen from "../../screens/SignIn/SignInOTPVerificationScreen";
 import SignInEmailVerificationScreen from "../../screens/SignIn/SignInEmailVerificationScreen";
 import SignInEmailScreen from "screens/SignIn/SignInEmailScreen";
+import COLORS from "constants/color";
+import HeaderBack from "components/HeaderBack";
 
 const Stack = createNativeStackNavigator<SignInNavigatorParamList>();
 
 const SignInNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={({ navigation }) => ({
+        headerStyle: { backgroundColor: COLORS.background },
+        headerShadowVisible: false,
+        headerBackVisible: false,
+        headerTitleStyle: { fontWeight: "bold" },
+        headerLeft: () => <HeaderBack onPress={() => navigation.goBack()} />,
+      })}
+      initialRouteName="SignInPhoneNumberScreen"
+    >
       <Stack.Screen
         name="SignInPhoneNumberScreen"
         component={SignInPhoneNumberScreen}
+        options={{
+          headerTitle: "Enter your phone number",
+        }}
       />
       <Stack.Screen
         name="SignInOTPVerificationScreen"
         component={SignInOTPVerificationScreen}
+        options={{
+          headerTitle: "Enter your code",
+        }}
       />
-      <Stack.Screen name="SignInEmailScreen" component={SignInEmailScreen} />
+      <Stack.Screen
+        name="SignInEmailScreen"
+        component={SignInEmailScreen}
+        options={{
+          headerTitle: "Enter your email",
+        }}
+      />
       <Stack.Screen
         name="SignInEmailVerificationScreen"
         component={SignInEmailVerificationScreen}
+        options={{
+          headerTitle: "Enter your code",
+        }}
       />
     </Stack.Navigator>
   );
