bdf4445393ff953e77d69403c2913e997b506de3
Ashutosh-Tiwari <ashutiwari9518@gmail.com>
2025-01-03 17:23:39 +0530

    #feat: add validation for sign up email screen, email otp screen and other minor fixes

--- src/screens/SignUp/SignUpEmailVerificationScreen.tsx
+++ src/screens/SignUp/SignUpEmailVerificationScreen.tsx
@@ -4,26 +4,54 @@ import ScreenWrapper from "components/ScreenWrapper";
 import { SignUpNavProps } from "navigation/SignUp/paramList";
 import React from "react";
 import { StyleSheet } from "react-native";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { codeSchema } from "src/utils/validationSchemas";
+
+interface FormikData {
+  code: string;
+}
+
+const schema = yup.object({
+  code: codeSchema,
+});
 
 const SignUpEmailVerificationScreen = ({
   navigation,
   route,
 }: SignUpNavProps<"SignUpEmailVerificationScreen">) => {
-  const [verificationCode, setVerificationCode] = React.useState("");
-
-  const handleVerify = () => {
-    navigation.navigate("SignUpNameScreen");
-  };
+  const formik = useFormik<FormikData>({
+    initialValues: {
+      code: "",
+    },
+    validationSchema: schema,
+    onSubmit: async () => {
+      // TODO: API for OTP verify
+      navigation.navigate("SignUpNameScreen");
+    },
+  });
 
   return (
     <ScreenWrapper>
       <CustomInput
         placeholder="Enter your code"
-        value={verificationCode}
-        onChangeText={setVerificationCode}
         keyboardType="numeric"
+        value={formik.values.code}
+        onChangeText={formik.handleChange("code")}
+        onBlur={formik.handleBlur("code")}
+        maxLength={6}
+        error={!!(formik.touched.code && formik.errors.code)}
+        helperText={
+          formik.touched.code && formik.errors.code
+            ? formik.errors.code
+            : undefined
+        }
       />
-      <CustomButton title="Next" style={styles.button} onPress={handleVerify} />
+      <CustomButton
+        title="Next"
+        style={styles.button}
+        onPress={formik.handleSubmit}
+      />
     </ScreenWrapper>
   );
 };
